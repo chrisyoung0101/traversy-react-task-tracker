3de866dd3057ad103095ff3bdd510b37acd12936
Chris Young <chrisyoung0101@gmail.com>
2021-06-08 21:28:08 -0500

    commit this

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -4,8 +4,13 @@ const Tasks = ({ tasks, onDelete, onToggle }) => {
   return (
     <>
       {/* using index here as when we used task.id we got the "needs a key" error */}
-      {tasks.map((task, index) => (
-        <Task key={index} task={task} onDelete={onDelete} onToggle={onToggle} />
+      {tasks.map((task) => (
+        <Task
+          key={task.id}
+          task={task}
+          onDelete={onDelete}
+          onToggle={onToggle}
+        />
       ))}
     </>
   );
